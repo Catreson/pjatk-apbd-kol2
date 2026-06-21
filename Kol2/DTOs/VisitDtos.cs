bc51f4876cb2eff0dab44b782a633390408529d5
Catreson <arekdagiel@gmail.com>
2026-06-21 16:58:37 +0200

    Remove unused

--- Kol2/DTOs/VisitDtos.cs
+++ Kol2/DTOs/VisitDtos.cs
@@ -47,16 +47,6 @@ public class VisitCreateDto
     public List<ServiceCreateDto> Services { get; set; } = [];
 }
 
-public class CustomerInfoDto
-{
-    [Required, MaxLength(50)]
-    public string FirstName { get; set; } = null!;
-    [Required, MaxLength(100)]
-    public string LastName { get; set; } = null!;
-    [MaxLength(100)]
-    public string? PhoneNumber { get; set; }
-}
-
 public class VisitServiceCreateDto
 {
     [Range(0.01, double.MaxValue)]
